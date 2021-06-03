14e602adf8ddd3cf78de897b541e0a7fbce831f3
Shadow <william.shadow@outlook.com>
2021-06-03 23:15:49 +0000

    init

---
+++ src/ids.js
@@ -0,0 +1,11 @@
+/*
+
+This file holds all the icons for a role.
+Generally lines should only be added by Shadow, but you
+can update the role IDs by simply replacing them.
+
+
+*/
+
+module.exports = {
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/*
+
+This file complies all the config files into one config object for the bot
+Nothing in this file should be adjusted unless instructed by Shadow
+
+*/
+module.exports = {
+  fn: require("./fn.js"),
+  ids: require("./ids.js"),
+}
